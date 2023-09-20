ca24dbb6bb3a102c471df7c93d659f384eae9921
Jelle-1035608 <Benardjelle@gmail.com>
2023-09-20 14:54:15 +0200

    ADD: azure datalake credentials

---
+++ credentials/AzureDataLake.credentials.ts
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AzureDataLakeApi = void 0;
+class AzureDataLakeApi {
+
+	name: string;
+	displayName: string;
+	documentationUrl: string;
+	properties: any;
+
+    constructor() {
+        this.name = 'azureDataLakeApi';
+        this.displayName = 'Azure Data Lake API';
+        this.documentationUrl = 'https://learn.microsoft.com/en-us/azure/storage/common/storage-account-keys-manage?toc=%2Fazure%2Fstorage%2Fblobs%2Ftoc.json&bc=%2Fazure%2Fstorage%2Fblobs%2Fbreadcrumb%2Ftoc.json&tabs=azure-portal#view-account-access-keys';
+        this.properties = [
+            {
+                displayName: 'Connection string',
+                name: 'connectionString',
+                type: 'string',
+                default: '',
+            },
+        ];
+    }
+}
+exports.AzureDataLakeApi = AzureDataLakeApi;
