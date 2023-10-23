00d3693a4746e973b61e26118670f1f81f614840
rico-0989903 <0989903@hr.nl>
2023-10-23 11:30:57 +0200

    ADD: First version for Node description

---
+++ nodes/Etherscan/Etherscan.node.ts
@@ -0,0 +1,33 @@
+import { INodeType, INodeTypeDescription } from 'n8n-workflow';
+
+export class Etherscan implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: "Etherscan API",
+		name: "Etherscan",
+		description: "Query the Etherscan API",
+		group: ["transform"],
+		version: 1,
+		defaults: {
+			name: "Etherscan",
+		},
+		inputs: ["main"],
+		outputs: ["main"],
+		credentials: [
+			{
+				// eslint-disable-next-line n8n-nodes-base/node-class-description-credentials-name-unsuffixed
+				name: "Etherscan",
+				required: true,
+			},
+		],
+		requestDefaults: {
+			baseURL: "https://api.etherscan.io/api",
+			headers: {
+				Accept: "application/json",
+				'Content-Type': 'application/json'
+			}
+		},
+		properties: [
+
+		]
+	};
+}
