c8c62c45e53953a71a63ee16c8d0dba67196dc7c
Jelle-1035608 <Benardjelle@gmail.com>
2023-10-25 23:37:45 +0200

    CHANGE: minor fixes in names

--- nodes/Etherscan/Etherscan.node.ts
+++ nodes/Etherscan/Etherscan.node.ts
@@ -35,15 +35,16 @@ export class Etherscan implements INodeType {
 				noDataExpression: true,
 				options: [
 					{
-						name: 'ABI for Varified Contract',
+						name: 'Get Contract ABI for Verified Contract',
 						value: 'getContractABI',
 					},
 					{
-						name: 'Get Source Code From Varified Contract',
+						name: 'Get Source Code for Verified Contract',
 						value: 'getSourceCode',
 					},
 					{
-						name: 'Get Contract Creator and Creation Tx Hash',
+						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
+						name: 'Get Contract Creator and create Tx Hash',
 						value: 'getTxHash',
 					},
 				],
@@ -157,7 +158,7 @@ export class Etherscan implements INodeType {
 						value: 'get',
 						// eslint-disable-next-line n8n-nodes-base/node-param-operation-option-action-miscased
 						action: 'Get Tx Hash',
-						description: "Returns a contract's deployer address and transaction hash it was created, up to 5 at a time",
+						description: "Returns a contract's deployer address and transaction hash it was created to, up to 5 at a time",
 						routing: {
 							request: {
 								method: 'GET',
