14c4999c084f742445b272f8835a24f7615c190f
Jelle-1035608 <Benardjelle@gmail.com>
2023-10-25 11:00:28 +0200

    ADD: added operations for the etherscan API

--- nodes/Etherscan/Etherscan.node.ts
+++ nodes/Etherscan/Etherscan.node.ts
@@ -47,7 +47,65 @@ export class Etherscan implements INodeType {
 					},
 				],
 				default: 'getContractABI' ,
-			}
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: [
+							'getContractABI',
+						],
+					},
+				},
+				options: [
+					{
+						name: 'Get',
+						value: 'get',
+						// eslint-disable-next-line n8n-nodes-base/node-param-operation-option-action-miscased
+						action: 'Get Contract ABI',
+						description: 'Returns the Contract Application Binary Interface ( ABI ) of a verified smart contract',
+						routing: {
+							request: {
+								method: 'GET',
+								url: '?module=contract&action=getabi&address=0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413&apikey={{$node["Etherscan"].credentials.apiKey}}',
+							},
+						},
+					},
+				],
+				default: 'get',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: [
+							'getTxHash',
+						],
+					},
+				},
+				options: [
+					{
+						name: 'Get',
+						value: 'get',
+						// eslint-disable-next-line n8n-nodes-base/node-param-operation-option-action-miscased
+						action: 'Get Tx Hash',
+						description: "Returns a contract's deployer address and transaction hash it was created, up to 5 at a time",
+						routing: {
+							request: {
+								method: 'GET',
+								url: '?module=contract&action=getcontractcreation&contractaddresses=0xB83c27805aAcA5C7082eB45C868d955Cf04C337F,0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45,0xe4462eb568E2DFbb5b0cA2D3DbB1A35C9Aa98aad,0xdAC17F958D2ee523a2206206994597C13D831ec7,0xf5b969064b91869fBF676ecAbcCd1c5563F591d0&apikey=YourApiKeyToken {{$node["Etherscan"].credentials.apiKey}}',
+							},
+						},
+					},
+				],
+				default: 'get',
+			},
 		]
 	};
 }
