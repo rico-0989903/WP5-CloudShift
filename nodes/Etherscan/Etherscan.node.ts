c9c966e61a29d17883ce91749e7a3bd6206a2211
rico-0989903 <0989903@hr.nl>
2023-10-25 11:21:55 +0200

    HOTFIX: URL fixed

--- nodes/Etherscan/Etherscan.node.ts
+++ nodes/Etherscan/Etherscan.node.ts
@@ -127,7 +127,7 @@ export class Etherscan implements INodeType {
 						routing: {
 							request: {
 								method: 'GET',
-								url: '?module=contract&action=getcontractcreation&contractaddresses=0xB83c27805aAcA5C7082eB45C868d955Cf04C337F,0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45,0xe4462eb568E2DFbb5b0cA2D3DbB1A35C9Aa98aad,0xdAC17F958D2ee523a2206206994597C13D831ec7,0xf5b969064b91869fBF676ecAbcCd1c5563F591d0&apikey=YourApiKeyToken {{$node["Etherscan"].credentials.apiKey}}',
+								url: '?module=contract&action=getcontractcreation&contractaddresses=0xB83c27805aAcA5C7082eB45C868d955Cf04C337F,0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45,0xe4462eb568E2DFbb5b0cA2D3DbB1A35C9Aa98aad,0xdAC17F958D2ee523a2206206994597C13D831ec7,0xf5b969064b91869fBF676ecAbcCd1c5563F591d0&apikey={{$node["Etherscan"].credentials.apiKey}}',
 							},
 						},
 					},
